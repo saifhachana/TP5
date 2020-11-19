7ccc642f57759c8db9c2afc041cc2029f6c0674a
Amel TRIKI <amel.triki.iset@gmail.com>
2020-11-19 16:37:58 +0100

    Composant listmateriel ajouté

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './composants/accueil/accueil.component';
+import { ListmaterielComponent } from './composants/informatique/listmateriel/listmateriel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    ListmaterielComponent
   ],
   imports: [
     BrowserModule
